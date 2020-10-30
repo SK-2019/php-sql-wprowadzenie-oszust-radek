2e669dfb79223dadf369904a9c033904d25f4575
oszust-radek <73523042+oszust-radek@users.noreply.github.com>
2020-10-30 14:53:07 +0100

    Update index.php

--- index.php
+++ index.php
@@ -57,6 +57,20 @@
         }
             echo("</table>");
 
+    $conn=new mysqli("mysql-oszust-radek.alwaysdata.net","217211","Radek003003%","oszust-radek_db");
+    $result=$conn->query('SELECT * FROM pracownicy where zarobki<45');
+    echo("<table border=1>");
+    echo("<th>id</th>");
+    echo("<th>imie</th>");
+    echo("<th>dzial</th>");
+    echo("<th>zarobki</th>");
+        while($row=$result->fetch_assoc()){
+            echo("<tr>");
+                echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
+                echo("</tr>");
+        }
+            echo("</table>");
+
 
 
 
