10372eefca586ac73600e45514942402f0e87115
oszust-radek <73523042+oszust-radek@users.noreply.github.com>
2020-10-30 14:46:47 +0100

    Update index.php

--- index.php
+++ index.php
@@ -13,23 +13,22 @@
                 echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
                 echo("</tr>");
         }
-            
+            echo("</table>");
+
+    $conn=new mysqli("mysql-oszust-radek.alwaysdata.net","217211","Radek003003%","oszust-radek_db");
+    $result=$conn->query('SELECT * FROM pracownicy');
+    echo("<table border=1>");
+    echo("<th>id</th>");
+    echo("<th>imie</th>");
+    echo("<th>dzial</th>");
+    echo("<th>zarobki</th>");
+        while($row=$result->fetch_assoc()){
+            echo("<tr>");
+                echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
+                echo("</tr>");
+        }
+            echo("</table>");
 
 
-                $conn=new mysqli("mysql-oszust-radek.alwaysdata.net","217211","Radek003003%","oszust-radek_db");
-            $result=$conn->query('SELECT * FROM pracownicy where dzial=2 or dzial=3');
-            echo("<table_border=1>");
-            echo("<th>id</th>");
-            echo("<th>imie</th>");
-            echo("<th>dzial</th>");
-            echo("<th>zarobki</th>");
-                while($row=$result->fetch_assoc()){
-                    echo("<tr>");
-                        echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
-                        echo("</tr>");
-                }
-                echo("</table>");
 
-                
-
-?>
+              ?>
