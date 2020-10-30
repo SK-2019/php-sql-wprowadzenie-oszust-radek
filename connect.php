701ebe715ae6fc5a5d28c611cf3248df96e7c61f
oszust-radek <73523042+oszust-radek@users.noreply.github.com>
2020-10-30 15:23:54 +0100

    Create connect.php

---
+++ connect.php
@@ -0,0 +1,16 @@
+<?php
+  require_once("connect.php");
+
+$servername = "mysql-oszust-radek.alwaysdata.net";
+$username = "217211";
+$password = "Radek003003%";
+$dbname = "oszust-radek_db";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+?>
