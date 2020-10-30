ee8624412de7b543df5f7d603dc7c0829a51b01c
oszust-radek <73523042+oszust-radek@users.noreply.github.com>
2020-10-30 14:42:57 +0100

    Update index.php

--- index.php
+++ index.php
@@ -13,35 +13,8 @@
                 echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
                 echo("</tr>");
         }
-        echo("</table>");
+            echo("</table>");
 
-        $conn=new mysqli("");
-            $result=$conn->query('SELECT * FROM pracownicy where imie not like '%a'');
-            echo("<table_border=1>");
-            echo("<th>id</th>");
-            echo("<th>imie</th>");
-            echo("<th>dzial</th>");
-            echo("<th>zarobki</th>");
-                while($row=$result->fetch_assoc()){
-                    echo("<tr>");
-                        echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row[""]."</td><td>".$row["zarobki"]."</td>");
-                    echo("</tr>");
-                }
-                echo("</table>");
-
-                $conn=new mysqli("");
-            $result=$conn->query('SELECT * FROM pracownicy where zarobki>30');
-            echo("<table_border=1>");
-            echo("<th>id</th>");
-            echo("<th>imie</th>");
-            echo("<th>dzial</th>");
-            echo("<th>zarobki</th>");
-                while($row=$result->fetch_assoc()){
-                    echo("<tr>");
-                        echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row[""]."</td><td>".$row["zarobki"]."</td>");
-                    echo("</tr>");
-                }
-                echo("</table>");
 
                 $conn=new mysqli("");
             $result=$conn->query('SELECT * FROM pracownicy where dzial=2 or dzial=3');
