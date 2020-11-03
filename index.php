02954d3ca6d28223ad705ca67e5b25fc3bb8f17b
oszust-radek <73523042+oszust-radek@users.noreply.github.com>
2020-11-03 10:21:12 +0100

    Update index.php

--- index.php
+++ index.php
@@ -12,9 +12,9 @@
             require_once("connect.php");
 
             echo("<h2>Zad1</h2>");
-            echo("<h3>SELECT * FROM pracownicy</h3>");
+            echo("<h3>SELECT count(imie) as kjl, dzial, nazwa_dzial FROM pracownicy, organizacja</h3>");
             $conn=new mysqli("mysql-oszust-radek.alwaysdata.net","217211","Radek003003%","oszust-radek_db");
-            $result=$conn->query('SELECT * FROM pracownicy');
+            $result=$conn->query('SELECT count(imie) as kjl, dzial, nazwa_dzial FROM pracownicy, organizacja where dzial=id_org group by dzial');
             echo("<table border=1>");
             echo("<th>id</th>");
             echo("<th>imie</th>");
@@ -22,7 +22,7 @@
             echo("<th>zarobki</th>");
                 while($row=$result->fetch_assoc()){
                     echo("<tr>");
-                        echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
+                        echo("<td>".$row["kjl"]."</td><td>".$row["dzial"]."</td><td>".$row["nazwa_dzial"]."</td>");
                         echo("</tr>");
                 }
                     echo("</table>");
