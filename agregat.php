216e56538bc2705700a6979de870f453ee40f7b1
oszust-radek <73523042+oszust-radek@users.noreply.github.com>
2020-11-05 11:14:28 +0100

    Update agregat.php

--- agregat.php
+++ agregat.php
@@ -1,3 +1,14 @@
-<?php
-echo("<h1>agregat.php</h1>");
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset-"UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+  <?php
+  echo("<h1>agregat.php</h1>");
+  ?>
+  </head>
+  </body>
