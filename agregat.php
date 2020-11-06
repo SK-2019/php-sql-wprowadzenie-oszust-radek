b9fc42384842d98ffd14132f3a15f8254855ea38
oszust-radek <73523042+oszust-radek@users.noreply.github.com>
2020-11-06 14:35:07 +0100

    Update agregat.php

--- agregat.php
+++ agregat.php
@@ -4,12 +4,69 @@
     <meta charset-"UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    <div class="nav"> 
+    <h2>Linki</h2>
+  <h4><a href="https://php-oszust-radek.herokuapp.com/"><b>STRONA GLOWNA</b></a></h4>  
+  <h4><a href="https://github.com/SK-2019/php-sql-wprowadzenie-oszust-radek"><b>GITHUB</b></a></h4>
+    <a href="orgPracownicy.php">ORGANIZACJA I PRACOWNICY</a>
+      <h6>   </h6>
+    <a href="agregat.php">FUNKCJE AGREGUJĄCE</a>
+      <h6>   </h6>
+    <a href="pracownicy.php">PRACOWNICY</a>
+</div>    
 </head>
 <body>
 
   <?php
   echo("<h1>agregat.php</h1>");
+    echo("<h2>Suma zarobkow wszystkich pracownikow</h2>");
+ $sql = "SELECT sum(zarobki) as sum FROM pracownicy, organizacja WHERE (dzial = id_org)";
+echo("<h3>".$sql."</h3>");
+$conn = new mysqli("remotemysql.com","F1aJmbwBvG","cmCvZxLITd","F1aJmbwBvG");
+ $result=$conn->query($sql);
+        echo("<table border=1>");
+     
+        echo("<th>sum</th>");
+            while($row=$result->fetch_assoc()) {
+                echo("<tr>");
+                    echo("<td>".$row["sum"]."</td>");
+                echo("</tr>");
+            }
+        echo("</table>");
+ 
+ echo("<h2>Suma zarobkow wszystkich kobiet</h2>");
+ $sql = "SELECT sum(zarobki) as sum FROM pracownicy, organizacja WHERE (dzial = id_org) and (imie like 'a%')";
+echo("<h3>".$sql."</h3>");
+$conn = new mysqli("remotemysql.com","F1aJmbwBvG","cmCvZxLITd","F1aJmbwBvG");
+ $result=$conn->query($sql);
+        echo("<table border=1>");
+     
+        echo("<th>sum</th>");
+            while($row=$result->fetch_assoc()) {
+                echo("<tr>");
+                    echo("<td>".$row["sum"]."</td>");
+                echo("</tr>");
+            }
+        echo("</table>");
+ 
+ echo("<h2>Suma zarobkow mezczyzn pracujacych w dziale 2 i 3</h2>");
+ $sql = "SELECT sum(zarobki) as sum FROM pracownicy, organizacja WHERE (dzial = id_org and imie not like 'a%') and (dzial = 2 or dzial = 3)";
+echo("<h3>".$sql."</h3>");
+$conn = new mysqli("remotemysql.com","F1aJmbwBvG","cmCvZxLITd","F1aJmbwBvG");
+ $result=$conn->query($sql);
+        echo("<table border=1>");
+     
+        echo("<th>sum</th>");
+            while($row=$result->fetch_assoc()) {
+                echo("<tr>");
+                    echo("<td>".$row["sum"]."</td>");
+                echo("</tr>");
+            }
+        echo("</table>");
   ?>
     
 </body>
 </html>
+
+
+
