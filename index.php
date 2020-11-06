4f15c9f02d781651ade0a21d111993b9973b15a6
oszust-radek <73523042+oszust-radek@users.noreply.github.com>
2020-11-06 14:47:50 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,9 +9,9 @@
     <div class="nav">
         <h2>linki</h2>
         <h4><a href="https://github.com/SK-2019/php-sql-wprowadzenie-oszust-radek">Github</a></h4>
-        <a href="orgPracownicy.php">Org i Prac</a>
-        <a href="agregat.php">F. agregujace</a>
-        <a href="pracownicy.php">Pracownicy</a>
+        <h4><a href="orgPracownicy.php">Org i Prac</a></h4>
+        <h4><a href="agregat.php">F. agregujace</a></h4>
+        <h4><a href="pracownicy.php">Pracownicy</a></h4>
 </div>
         <?php
             echo("<h1>Radek Oszust</h1>");
