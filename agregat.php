f19b66d96f16b62441c42611e8be41b0503a54a6
oszust-radek <73523042+oszust-radek@users.noreply.github.com>
2020-11-06 15:04:25 +0100

    Update agregat.php

--- agregat.php
+++ agregat.php
@@ -16,7 +16,7 @@
 <body>
 
   <?php
-  echo("<h1>agregat.php</h1>");
+    echo("<h1>agregat.php</h1>");
     echo("<h2>Suma zarobkow wszystkich pracownikow</h2>");
  $sql = "SELECT sum(zarobki) as sum FROM pracownicy, organizacja WHERE (dzial = id_org)";
 echo("<h3>".$sql."</h3>");
