5d65b2023e452a602566f8928d2befcf956790c6
oszust-radek <73523042+oszust-radek@users.noreply.github.com>
2020-11-06 15:33:24 +0100

    Update orgPracownicy.php

--- orgPracownicy.php
+++ orgPracownicy.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Karolina Zatylny 2Ti</title>
+        <title>Radek Oszust</title>
         <link rel="stylesheet" href="style.css">
     </head>
 <body>
