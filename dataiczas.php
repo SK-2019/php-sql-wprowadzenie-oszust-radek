e187c42f25f14198d0939a6110a021d7a0008db1
oszust-radek <73523042+oszust-radek@users.noreply.github.com>
2020-11-10 10:03:13 +0100

    Update dataiczas.php

--- dataiczas.php
+++ dataiczas.php
@@ -1,9 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset-"UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <div class="nav"> 
+    <h2>linki</h2>
+        <h3>
+    <a href="https://php-oszust-radek.herokuapp.com/"><b>Str Glowna</b></a>
+    <a href="https://github.com/SK-2019/php-sql-wprowadzenie-oszust-radek"><b>Github | </b></a>
+    <a href="agregat.php"><b>F. agregujace | </b></a>
+    <a href="orgPracownicy.php"><b>Org i Prac | </b></a>
+    <a href="pracownicy.php"><b>Data i czas</b></a>
+        </h3>
+</div>    
+</head>
+<body>
+
 <?php
-
-
-
-
-
 
 echo("<h2>Wiek poszczególnych pracowników (w latach)</h2>");
  $sql = "SELECT * ,YEAR(curdate())-YEAR(data_urodzenia) AS wiek FROM pracownicy, organizacja WHERE (dzial = id_org)";
