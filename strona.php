469be1a9074e5ca59537f35cacfcb4e767146ca4
oszust-radek <73523042+oszust-radek@users.noreply.github.com>
2020-11-17 10:09:11 +0100

    Update strona.php

--- strona.php
+++ strona.php
@@ -1,5 +1,13 @@
+
+<html>
+  <head>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  
 <?php
   echo("strona.php");
 
   echo($_POST['name']);
+  echo($_POST['lastname']);
+  echo($_POST['country']);
 ?>
