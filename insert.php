cb6f5c8cadb094566834caddcb47c41fd22e1fa2
oszust-radek <73523042+oszust-radek@users.noreply.github.com>
2020-11-20 11:01:03 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,4 +1,4 @@
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli(mysql-oszust-radek.alwaysdata.net","217211","Radek003003%","oszust-radek_db);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
