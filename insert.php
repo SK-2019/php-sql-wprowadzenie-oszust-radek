c35ae4d91813825178dd6c321b726197b358a596
oszust-radek <73523042+oszust-radek@users.noreply.github.com>
2020-11-20 15:40:13 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -9,14 +9,14 @@
     
       require_once("connect.php");
 
-  $sql = "INSERT INTO Pracownik (null, name, dzial,zarobki,data_urodzenia)
+  $sql = "INSERT INTO Pracownik (id_pracownicy, name, dzial,zarobki,data_urodzenia)
           VALUES (
             null, 
-            $_POST['name'], 
-            $_POST['dzial'], 
-            $_POST['zarobki'],
-            $_POST['data_urodzenia']
-          ";
+            '.$_POST['name'].', 
+            '.$_POST['dzial'].', 
+            '.$_POST['zarobki'].',
+           '.$_POST['data_urodzenia'].'
+          )";
 
         echo "<li>". $sql;
 
