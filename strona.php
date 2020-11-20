dbb59df61f7bc43404b4024cffe7ffa237f1d570
oszust-radek <73523042+oszust-radek@users.noreply.github.com>
2020-11-20 11:13:45 +0100

    Update strona.php

--- strona.php
+++ strona.php
@@ -9,8 +9,8 @@
 <?php
   echo("<li> imie: ".$_POST['imie']."</li>");
   echo("<li> nazwisko: ".$_POST['nazwisko']."</li>");
-  echo("<li> kraj: ".$_POST['country']."</li>");
-        
+  echo("<li> kraj: ".$_POST['kraj']."</li>");
+  echo("<li> phone: ".$_POST['nr telefonu']."</li>");      
   
 ?>
     </div>  
