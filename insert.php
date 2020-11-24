b037757b0eea07b6fa15cad3e4b8fe885930ad18
oszust-radek <73523042+oszust-radek@users.noreply.github.com>
2020-11-24 09:30:55 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,6 +1,33 @@
 <?php
 
+echo("jestes w insert.php");
+echo "<li>". $_POST['name'];
+echo "<li>". $_POST['dzial'];
+echo "<li>". $_POST['zarobki'];
+echo "<li>". $_POST['data_urodzenia'];
 
+
+
+
+$conn = new mysqli();
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO pracownicy (id_pracownicy,imie, dzial, zarobki, data_urodzenia) 
+       VALUES (null,'".$_POST['name']."', ".$_POST['dzial'].", ".$_POST['zarobki'].",'".$_POST['data_urodzenia']."')";
+
+
+echo "<li>". $sql;
+
+if ($conn->query($sql) === TRUE) {
+  echo ("New record created successfully");
+} else {
+//informacja o ewentualnych błędach
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
       
 
   
