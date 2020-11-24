f54eea4bc6f7ff91bbbc1d90608bc6aecc4624ea
oszust-radek <73523042+oszust-radek@users.noreply.github.com>
2020-11-24 09:31:16 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -9,7 +9,7 @@ echo "<li>". $_POST['data_urodzenia'];
 
 
 
-$conn = new mysqli();
+$conn = new mysqli("mysql-oszust-radek.alwaysdata.net","217211","Radek003003%","oszust-radek_db");
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
