ed9956761554e87348ea20635b6b83185b132a39
oszust-radek <73523042+oszust-radek@users.noreply.github.com>
2020-11-24 09:27:33 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,14 +1,8 @@
 <?php
 
-require_once("connect.php");
-$sql =  "INSERT INTO `pracownicy`(`id_pracownicy`, `imie`, `dzial`, `zarobki`, `data_urodzenia`) VALUES (NULL,'".$_POST['imie']."','".$_POST['dzial']."','".$_POST['zarobki']."','".$_POST['data_']."')";
-echo($sql);
-mysqli_query($conn,$sql);
-mysqli_close($conn);
 
-?>
       
 
   
-
+?>
 
