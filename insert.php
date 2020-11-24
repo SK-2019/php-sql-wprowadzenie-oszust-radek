587f00c0e29d07184d5c36224f353db76e3c1380
oszust-radek <73523042+oszust-radek@users.noreply.github.com>
2020-11-24 09:18:41 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,29 +1,14 @@
 <?php
 require_once("connect.php");
 
-  echo("jestes w insert.php <br>");
-echo "<li>". $_POST['name'];
-echo "<li>". $_POST['dzial'];
-echo "<li>". $_POST['zarobki'];
-echo "<li>". $_POST['data_urodzenia'];
+  $sql = "INSERT INTO Pracownik (id, imie, dzial, zarobki,data_urodzenia) 
+       VALUES (null,'Balbina', 4, 86,'1999-05-21')";
 
-    $conn = new mysqli(mysql-oszust-radek.alwaysdata.net","217211","Radek003003%","oszust-radek_db);
-    
-$sql = "INSERT INTO Pracownik (null, name, dzial,zarobki,data_urodzenia)
-	      VALUES (
-				  null,". 
-					$_POST['name'].", 
-					".$_POST['dzial'].", 
-					".$_POST['zarobki'].",
-					".$_POST['data_urodzenia']."
-				;
-
-
-echo "<li>". $sql;
 
 if ($conn->query($sql) === TRUE) {
   echo "New record created successfully";
 } else {
+
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
