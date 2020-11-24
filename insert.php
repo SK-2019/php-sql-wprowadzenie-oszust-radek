2e7bcb4685ca959a6fb086fa588db51e5728b1a9
oszust-radek <73523042+oszust-radek@users.noreply.github.com>
2020-11-24 09:19:55 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,6 +1,8 @@
 <?php
 require_once("connect.php");
 
+$conn = new mysqli("mysql-oszust-radek.alwaysdata.net","217211","Radek003003%","oszust-radek_db");
+
   $sql = "INSERT INTO Pracownik (id, imie, dzial, zarobki,data_urodzenia) 
        VALUES (null,'Balbina', 4, 86,'1999-05-21')";
 
