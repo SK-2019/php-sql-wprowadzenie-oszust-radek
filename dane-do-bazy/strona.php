969b4ff162e0b5110fb7a5fa38cf62d4ee12d739
oszust-radek <73523042+oszust-radek@users.noreply.github.com>
2021-02-07 19:56:30 +0100

    Update strona.php

--- dane-do-bazy/strona.php
+++ dane-do-bazy/strona.php
@@ -1,1 +1,18 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+    <title>strona</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="s">
+<?php
+  echo("<li> imie: ".$_POST['firstname']."</li>");
+  echo("<li> nazwisko: ".$_POST['lastname']."</li>");
+  echo("<li> kraj: ".$_POST['country']."</li>");
+  echo("<li> telefon: ".$_POST['phone']."</li>");      
+  
+?>
+    </div>  
+</body>
+</html>
