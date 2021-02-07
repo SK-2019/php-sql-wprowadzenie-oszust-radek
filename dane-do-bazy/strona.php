89341e487874b4f1147f259f1844df59529d0ca1
oszust-radek <73523042+oszust-radek@users.noreply.github.com>
2021-02-07 20:01:06 +0100

    Update strona.php

--- dane-do-bazy/strona.php
+++ dane-do-bazy/strona.php
@@ -6,7 +6,17 @@
     <link rel="icon" href="https://www.favicon.cc/logo3d/308168.png">
 </head>
 <body>
-    <div class="s">
+        <div class="nav">
+        <h2>linki</h2>
+        <h4><a href="https://github.com/SK-2019/php-sql-wprowadzenie-oszust-radek">Github</a></h4>
+        <h4><a href="orgPracownicy.php">Org i Prac</a></h4>
+        <h4><a href="agregat.php">F. agregujace</a></h4>
+        <h4><a href="pracownicy.php">Pracownicy</a></h4>
+        <h4><a href="dataiczas.php">Data i czas</a></h4>
+        <h4><a href="strona.php">Strona</a></h4>
+        <a href="formularz.html">Formularz</a>
+        <h4><a href="daneDoBazy.html">Insert</a></h4>
+</div>
 <?php
   echo("<li> imie: ".$_POST['firstname']."</li>");
   echo("<li> nazwisko: ".$_POST['lastname']."</li>");
