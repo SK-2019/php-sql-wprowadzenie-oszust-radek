45428396696b9d1c4838a8915026e7508dab5641
oszust-radek <73523042+oszust-radek@users.noreply.github.com>
2021-02-07 20:08:44 +0100

    Update agregat.php

--- pracownicy/agregat.php
+++ pracownicy/agregat.php
@@ -1,1 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset-"UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <div class="nav"> 
+    <h2>linki</h2>
+        <h3>
+    <a href="https://php-oszust-radek.herokuapp.com/"><b>Str Glowna</b></a>
+    <a href="https://github.com/SK-2019/php-sql-wprowadzenie-oszust-radek"><b>Github | </b></a>
+    <a href="agregat.php"><b>F. agregujace | </b></a>
+    <a href="orgPracownicy.php"><b>Org i Prac | </b></a>
+    <a href="pracownicy.php"><b>Pracownicy</b></a>
+    <a href="dataiczas.php">Data i czas</b></a>
+        </h3>
+</div>    
+</head>
+<body>
+
+  <?php
+    echo("<h1>agregat.php</h1>");
+    echo("<h2>Suma zarobkow wszystkich pracownikow</h2>");
+    require_once("connect.php");
+ $sql = "SELECT sum(zarobki) as sum FROM pracownicy, organizacja WHERE (dzial = id_org)";
+echo("<h3>".$sql."</h3>");
+$conn = new mysqli("mysql-oszust-radek.alwaysdata.net","217211","Radek003003%","oszust-radek_db");
+ $result=$conn->query($sql);
+        echo("<table border=1>");
+     
+        echo("<th>sum</th>");
+            while($row=$result->fetch_assoc()) {
+                echo("<tr>");
+                    echo("<td>".$row["sum"]."</td>");
+                echo("</tr>");
+            }
+        echo("</table>");
+ 
+ echo("<h2>Suma zarobkow wszystkich kobiet</h2>");
+ $sql = "SELECT sum(zarobki) as sum FROM pracownicy, organizacja WHERE (dzial = id_org) and (imie like 'a%')";
+echo("<h3>".$sql."</h3>");
+$conn = new mysqli("mysql-oszust-radek.alwaysdata.net","217211","Radek003003%","oszust-radek_db");
+ $result=$conn->query($sql);
+        echo("<table border=1>");
+     
+        echo("<th>sum</th>");
+            while($row=$result->fetch_assoc()) {
+                echo("<tr>");
+                    echo("<td>".$row["sum"]."</td>");
+                echo("</tr>");
+            }
+        echo("</table>");
+ 
+ echo("<h2>Suma zarobkow mezczyzn pracujacych w dziale 2 i 3</h2>");
+ $sql = "SELECT sum(zarobki) as sum FROM pracownicy, organizacja WHERE (dzial = id_org and imie not like 'a%') and (dzial = 2 or dzial = 3)";
+echo("<h3>".$sql."</h3>");
+$conn = new mysqli("mysql-oszust-radek.alwaysdata.net","217211","Radek003003%","oszust-radek_db");
+ $result=$conn->query($sql);
+        echo("<table border=1>");
+     
+        echo("<th>sum</th>");
+            while($row=$result->fetch_assoc()) {
+                echo("<tr>");
+                    echo("<td>".$row["sum"]."</td>");
+                echo("</tr>");
+            }
+        echo("</table>");
+  ?>
+    
+</body>
+</html>
 
