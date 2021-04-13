b5778dd6e578e9a5268916ddd351d8aa5e3ac45f
oszust-radek <73523042+oszust-radek@users.noreply.github.com>
2021-04-13 10:05:58 +0200

    Update pracownicy.php

--- pracownicy/pracownicy.php
+++ pracownicy/pracownicy.php
@@ -7,17 +7,24 @@
         <link rel="icon" href="https://www.favicon.cc/logo3d/308168.png">
     </head>
 <body>
-    <div class="nav">
-        <h2>linki</h2>
-        <h4><a href="https://github.com/SK-2019/php-sql-wprowadzenie-oszust-radek">Github</a></h4>
-        <h4><a href="orgPracownicy.php">Org i Prac</a></h4>
-        <h4><a href="agregat.php">F. agregujace</a></h4>
-        <h4><a href="pracownicy.php">Pracownicy</a></h4>
-        <h4><a href="dataiczas.php">Data i czas</a></h4>
-        <h4><a href="strona.php">Strona</a></h4>
-        <a href="formularz.html">Formularz</a>
-        <h4><a href="daneDoBazy.html">Insert</a></h4>
+    <div class="container">
+    <div class="item colorRed">
+        <h1 class="title">Radek Oszust</h1>
 </div>
+<div class="item colorBlue">
+    <div class="nav">
+        
+        <li class="nav_item"><a href="https://github.com/SK-2019/php-sql-wprowadzenie-oszust-radek">Github</a></li>
+        <li class="nav_item"><a href="/pracownicy/orgPracownicy.php">Org i Prac</a></li>
+        <li class="nav_item"><a href="/pracownicy/agregat.php">F. agregujace</a></li>
+        <li class="nav_item"><a href="/pracownicy/pracownicy.php">Pracownicy</a></li>
+        <li class="nav_item"><a href="/pracownicy/dataiczas.php">Data i czas</a></li>
+        <li class="nav_item"><a href="/dane-do-bazy/strona.php">Strona</a></li>
+        <li class="nav_item"><a href="/dane-do-bazy/formularz.html">Formularz</a></li>
+        <li class="nav_item"><a href="/dane-do-bazy/daneDoBazy.html">Insert</a></li>
+        </ul>
+</div>
+<div class="item colorGreen"></div>
 
 
 <?php
