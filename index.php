1f766b6fd180c2807a7915a8ebc77d5b6b1cb993
oszust-radek <73523042+oszust-radek@users.noreply.github.com>
2021-04-13 09:55:50 +0200

    Update index.php

--- index.php
+++ index.php
@@ -19,6 +19,7 @@
         <li class="nav_item"><a href="/pracownicy/agregat.php">F. agregujace</a></li>
         <li class="nav_item"><a href="/pracownicy/pracownicy.php">Pracownicy</a></li>
         <li class="nav_item"><a href="/pracownicy/dataiczas.php">Data i czas</a></li>
+        <li class="nav_item"><a href="flexbox/flexbox.html">Flexbox</a></li>
         <li class="nav_item"><a href="/dane-do-bazy/strona.php">Strona</a></li>
         <li class="nav_item"><a href="/dane-do-bazy/formularz.html">Formularz</a></li>
         <li class="nav_item"><a href="/dane-do-bazy/daneDoBazy.html">Insert</a></li>
