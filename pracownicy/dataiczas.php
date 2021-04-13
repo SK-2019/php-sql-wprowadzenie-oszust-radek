f3315016555f1cda0bcfbfc423ca90e347a92ac4
oszust-radek <73523042+oszust-radek@users.noreply.github.com>
2021-04-13 10:21:33 +0200

    Update dataiczas.php

--- pracownicy/dataiczas.php
+++ pracownicy/dataiczas.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset-"UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/assests/style.css">
+    <link rel="stylesheet" href="/assets/style.css">
     <link rel="icon" href="https://www.favicon.cc/logo3d/308168.png">
     
     <div class="container">
