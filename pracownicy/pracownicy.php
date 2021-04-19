d64664c5562d4149fa37bfde74cb6a8bae8833b1
oszust-radek <73523042+oszust-radek@users.noreply.github.com>
2021-04-19 21:58:42 +0200

    Update pracownicy.php

--- pracownicy/pracownicy.php
+++ pracownicy/pracownicy.php
@@ -15,7 +15,7 @@
     <div class="nav">
        
         <li class="nav_item"><a href="https://github.com/SK-2019/php-sql-wprowadzenie-oszust-radek">Github</a></li>
-        <li class="nav_item"><a href="index.php">Strona główna</a></li>
+        <li class="nav_item"><a href="/index.php">Strona główna</a></li>
         <li class="nav_item"><a href="/pracownicy/orgPracownicy.php">Org i Prac</a></li>
         <li class="nav_item"><a href="/pracownicy/agregat.php">F. agregujace</a></li>
         <li class="nav_item"><a href="/pracownicy/dataiczas.php">Data i czas</a></li>
