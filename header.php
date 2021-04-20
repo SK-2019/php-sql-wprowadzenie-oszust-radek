50fff420cb9e0a6a86f0691c293f88776a4e7905
oszust-radek <73523042+oszust-radek@users.noreply.github.com>
2021-04-20 11:22:55 +0200

    Update header.php

--- header.php
+++ header.php
@@ -1,2 +1,2 @@
 <h1 class="title">Radek Oszust</h1>
-<a href="https://www.notion.so/SQL-3ccfadeab4b84d7794d7a13966de2547">Notion</a>
+<li href="https://www.notion.so/SQL-3ccfadeab4b84d7794d7a13966de2547">Notion</a></li>
