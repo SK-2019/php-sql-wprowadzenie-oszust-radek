94a51ffe7dd6133df53583973a8a5a04c242246a
oszust-radek <73523042+oszust-radek@users.noreply.github.com>
2021-04-20 09:55:25 +0200

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,5 +1,5 @@
-        <a class="nav_item"><a href="https://www.notion.so/SQL-3ccfadeab4b84d7794d7a13966de2547">Notion</a>
-        <li class="nav_item"><a href="https:/github.com/SK-2019/php-sql-wprowadzenie-oszust-radek">Github</a></li>
+
+         <li class="nav_item"><a href="https:/github.com/SK-2019/php-sql-wprowadzenie-oszust-radek">Github</a></li>
         <li class="nav_item"><a href="/pracownicy/orgPracownicy.php">Org i Prac</a></li>
         <li class="nav_item"><a href="/pracownicy/agregat.php">F. agregujace</a></li>
         <li class="nav_item"><a href="/pracownicy/pracownicy.php">Pracownicy</a></li>
