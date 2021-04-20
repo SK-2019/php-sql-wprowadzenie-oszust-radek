1e073d03d1367f3349bd78d64adfe7ebd60d2ee0
oszust-radek <73523042+oszust-radek@users.noreply.github.com>
2021-04-20 10:50:21 +0200

    Create wieledowielu.php

---
+++ biblioteka/wieledowielu.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="/assets/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="/inne/favicon.ico">
+</head>
+
+
+
+
+<div class="con">
+<?php
+                echo("<h1>Radek Oszust</h1>");
+
+                echo("<h2>zad 1</h2>");
+
+                require_once("../connect.php");
+
+                $sql = ("SELECT * FROM autor, tytul, autor_tytul  where autor_id = autor.id and tytul_id = tytul.id");
+                echo("<h2>".$sql."</h2>");
+                $result=$conn->query($sql);
+                
+                        echo("<table border=1>");
+                        echo("<th>autor</th>");
+                        echo("<th>tytul</th>");
+    
+
+                        while($row=$result->fetch_assoc()) {
+                                echo("<tr>");
+                                    echo("<td>".$row["nazwisko"]."</td><td>".$row["tytul"]."</td>");
+                                echo("</tr>");
+                            }
+                        echo("</table>");
+
+
+                        ?>
+</div>
