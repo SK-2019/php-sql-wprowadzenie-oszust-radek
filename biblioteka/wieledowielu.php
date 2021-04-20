eae5b2e3d4169341f996e507b1b3d713c88aea1c
oszust-radek <73523042+oszust-radek@users.noreply.github.com>
2021-04-20 11:10:37 +0200

    Update wieledowielu.php

--- biblioteka/wieledowielu.php
+++ biblioteka/wieledowielu.php
@@ -18,7 +18,7 @@
 
                 echo("<h2>zad 1</h2>");
 
-                require_once("connect.php");
+                require_once("../connect.php");
 
                 $sql = ("SELECT * FROM autor, tytul, autor_tytul  where autor_id = autor.id and tytul_id = tytul.id");
                 echo("<h2>".$sql."</h2>");
