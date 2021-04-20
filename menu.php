42f9a86fbee5e7ae0368422623db9378da488fcf
oszust-radek <73523042+oszust-radek@users.noreply.github.com>
2021-04-20 09:25:19 +0200

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,1 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset-"UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="https://www.favicon.cc/logo3d/308168.png">
+    <link rel="stylesheet" href="/assets/style.css">
+</head>
+<body>
+
+       <?php include("menu.php) ?>
+        <li class="nav_item"><a href="https:/github.com/SK-2019/php-sql-wprowadzenie-oszust-radek">Github</a></li>
+        <li class="nav_item"><a href="/pracownicy/orgPracownicy.php">Org i Prac</a></li>
+        <li class="nav_item"><a href="/pracownicy/agregat.php">F. agregujace</a></li>
+        <li class="nav_item"><a href="/pracownicy/pracownicy.php">Pracownicy</a></li>
+        <li class="nav_item"><a href="/pracownicy/dataiczas.php">Data i czas</a></li>
+        <li class="nav_item"><a href="flexbox/flexbox.html">Flexbox</a></li>
+        <li class="nav_item"><a href="/dane-do-bazy/strona.php">Strona</a></li>
+        <li class="nav_item"><a href="/dane-do-bazy/formularz.html">Formularz</a></li>
+        <li class="nav_item"><a href="/dane-do-bazy/daneDoBazy.html">Insert</a></li>
+       
+      </body>
+      </html>
 
