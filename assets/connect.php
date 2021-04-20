9129f62a65d493c1849ad7b3d24fa68abcf1c8e7
oszust-radek <73523042+oszust-radek@users.noreply.github.com>
2021-04-20 10:57:45 +0200

    Delete connect.php

--- assets/connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once("config.php");
-$conn = new mysqli($_SERVER['MYSQL_SERV'],$_SERVER['MYSQL_NAME'], $_SERVER['MYSQL_PS'], $_SERVER['MYSQL_DB']);
-if ($conn->connect_error) {
-    die("connection failed: ".mysqli_connect_error());
-}
-?>
