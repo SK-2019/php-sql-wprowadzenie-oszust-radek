828e3758c064badf75570b8bb4434502e4a1a8c2
oszust-radek <73523042+oszust-radek@users.noreply.github.com>
2021-04-20 09:36:41 +0200

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,21 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset-"UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="https://www.favicon.cc/logo3d/308168.png">
-    <link rel="stylesheet" href="/assets/style.css">
-</head>
-<body>
-
-      <div class="container">
-    <div class="item colorRed">
-</div>
-<div class="item colorBlue">
-    <div class="nav">
-        
-      
-        <li class="nav_item"><a href="https:/github.com/SK-2019/php-sql-wprowadzenie-oszust-radek">Github</a></li>
+ <li class="nav_item"><a href="https:/github.com/SK-2019/php-sql-wprowadzenie-oszust-radek">Github</a></li>
         <li class="nav_item"><a href="/pracownicy/orgPracownicy.php">Org i Prac</a></li>
         <li class="nav_item"><a href="/pracownicy/agregat.php">F. agregujace</a></li>
         <li class="nav_item"><a href="/pracownicy/pracownicy.php">Pracownicy</a></li>
@@ -24,11 +7,3 @@
         <li class="nav_item"><a href="/dane-do-bazy/strona.php">Strona</a></li>
         <li class="nav_item"><a href="/dane-do-bazy/formularz.html">Formularz</a></li>
         <li class="nav_item"><a href="/dane-do-bazy/daneDoBazy.html">Insert</a></li>
-       
-</div>
-<div class="item colorGreen"></div>
-</div>  
-       
-      </body>
-      </html>
-
