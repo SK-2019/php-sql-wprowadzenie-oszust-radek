ec8409e392d3a3ac9eb32d8707d8f4c13d951147
oszust-radek <73523042+oszust-radek@users.noreply.github.com>
2021-04-20 09:39:30 +0200

    Update index.php

--- index.php
+++ index.php
@@ -15,16 +15,7 @@
     <div class="nav">
         
        <?php include("menu.php"); ?>
-        <li class="nav_item"><a href="https:/github.com/SK-2019/php-sql-wprowadzenie-oszust-radek">Github</a></li>
-        <li class="nav_item"><a href="/pracownicy/orgPracownicy.php">Org i Prac</a></li>
-        <li class="nav_item"><a href="/pracownicy/agregat.php">F. agregujace</a></li>
-        <li class="nav_item"><a href="/pracownicy/pracownicy.php">Pracownicy</a></li>
-        <li class="nav_item"><a href="/pracownicy/dataiczas.php">Data i czas</a></li>
-        <li class="nav_item"><a href="flexbox/flexbox.html">Flexbox</a></li>
-        <li class="nav_item"><a href="/dane-do-bazy/strona.php">Strona</a></li>
-        <li class="nav_item"><a href="/dane-do-bazy/formularz.html">Formularz</a></li>
-        <li class="nav_item"><a href="/dane-do-bazy/daneDoBazy.html">Insert</a></li>
-       
+   
 </div>
 <div class="item colorGreen"></div>
 </div>
