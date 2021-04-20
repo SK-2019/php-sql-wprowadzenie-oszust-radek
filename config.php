5dd7c116536a9ea13eb74bc64cb375b4788c1382
oszust-radek <73523042+oszust-radek@users.noreply.github.com>
2021-04-20 11:08:44 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+$_SERVER['servername'] = "mysql-oszust-radek.alwaysdata.net";
+$_SERVER['username'] = "217211";
+$_SERVER['password'] = "Radek003003%";
+$_SERVER['dbname'] = "oszust-radek_db";
+?>
